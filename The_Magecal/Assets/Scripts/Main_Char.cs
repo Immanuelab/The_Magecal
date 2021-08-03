37953bff4c95bc81f365c9ab1798197a4979011d
underscore_underscore_underscore <choj@stu.otc.school.nz>
2021-08-04 09:40:58 +1200

    Added movement speed

--- The_Magecal/Assets/Scripts/Main_Char.cs
+++ The_Magecal/Assets/Scripts/Main_Char.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class Main_Char : MonoBehaviour
 {
+    public float movespeed = 3;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("Left");
             transform.localScale = new Vector3(1, 1, 1);
-            transform.Translate(Vector3.left * Time.deltaTime);
+            transform.Translate(Vector3.left * movespeed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             Debug.Log("Right");
             transform.localScale = new Vector3(-1, 1, 1);
-            transform.Translate(Vector3.right * Time.deltaTime);
+            transform.Translate(Vector3.right * movespeed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Space bar");
         }
