30e7cae9af614a9c3c784bf060405aff7a5ce1f1
underscore_underscore_underscore <choj@stu.otc.school.nz>
2021-08-06 11:00:35 +1200

    Attack animation

--- The_Magecal/Assets/Scripts/Main_Char.cs
+++ The_Magecal/Assets/Scripts/Main_Char.cs
@@ -7,6 +7,7 @@ public class Main_Char : MonoBehaviour
     public float movespeed = 3;
     // Start is called before the first frame update
     Animator animator;
+    public bool dynamic = false;
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -15,26 +16,39 @@ public class Main_Char : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) || !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            dynamic = false;
+        }
+        else if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("Left");
             transform.localScale = new Vector3(1, 1, 1);
             transform.Translate(Vector3.left * movespeed * Time.deltaTime);
-            animator.SetBool("run", true);
+            dynamic = true;
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             Debug.Log("Right");
             transform.localScale = new Vector3(-1, 1, 1);
             transform.Translate(Vector3.right * movespeed * Time.deltaTime);
-            animator.SetBool("run", true);
+            dynamic = true;
         }
-        if (Input.GetKey(KeyCode.Space))
+        else if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Space bar");
         }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            Debug.Log("Attack");
+            animator.SetTrigger("atk");
+        }
+
+
+        if (dynamic == false)
+        {animator.SetBool("run", false);}
         else {
-            animator.SetBool("run", false);
+            animator.SetBool("run", true);
         }
     }
 }
