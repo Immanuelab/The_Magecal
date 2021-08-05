370736bcd30ebe2c699372db6223d33c6da07ca7
underscore_underscore_underscore <choj@stu.otc.school.nz>
2021-08-05 13:30:32 +1200

    Changed character

--- The_Magecal/Assets/Scripts/Main_Char.cs
+++ The_Magecal/Assets/Scripts/Main_Char.cs
@@ -6,8 +6,10 @@ public class Main_Char : MonoBehaviour
 {
     public float movespeed = 3;
     // Start is called before the first frame update
+    Animator animator;
     void Start()
     {
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,16 +20,21 @@ public class Main_Char : MonoBehaviour
             Debug.Log("Left");
             transform.localScale = new Vector3(1, 1, 1);
             transform.Translate(Vector3.left * movespeed * Time.deltaTime);
+            animator.SetBool("run", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             Debug.Log("Right");
             transform.localScale = new Vector3(-1, 1, 1);
             transform.Translate(Vector3.right * movespeed * Time.deltaTime);
+            animator.SetBool("run", true);
         }
         if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Space bar");
+        }
+        else {
+            animator.SetBool("run", false);
         }
     }
 }
